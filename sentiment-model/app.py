00bd36644a1f434fbf6d19d85779d417592f1509
getoxdev <getoxdev@gmail.com>
2025-04-13 00:04:13 -0600

    completed api

--- sentiment-model/app.py
+++ sentiment-model/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+
+from views import get_para_sentiments
 
 app = Flask(__name__)
 
@@ -6,6 +8,20 @@ app = Flask(__name__)
 def index():
     data = "Hello World"
     return jsonify(data)
+
+@app.route('/para-sentiment-analyze', methods=['POST'])
+def para_sentiment_analyze():
+    try:
+        data = request.get_json()
+        if not isinstance(data, list) or not all(isinstance(item, str) for item in data):
+            return jsonify({"error": "Input must be a list of strings"}), 400
+
+        sentiments = get_para_sentiments(data)
+        return jsonify(sentiments)
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run()
